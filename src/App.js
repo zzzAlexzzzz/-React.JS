446fecf64d2215c695e285b3829323f761ed1d08
zzzAlexzzzz <hudorozhkov1985@mail.ru>
2023-05-18 22:38:38 +0500

    Занятие 2. Классовые компоненты

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Bla from "./Bla";
+
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const hendleInc = () => {
+    setCounter(counter + 1)
+  }
+  const hendleDec = () => {
+    if (counter === 0) {
+      return
+    }
+    setCounter(counter - 1)
+    
+  }
+  const nuller = () => {
+    setCounter(0)
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> {counter} </h1>
+      <button onClick={hendleInc}>+</button>
+      <button onClick={hendleDec}>-</button>
+      <button onClick={nuller}>Сброс</button>
     </div>
   );
 }
---
+++ src/Bla.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Bla() {
+return (
+<div></div>
+)
+}
+
